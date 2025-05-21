1cb66fd256e32aa97241cb4e711026a363bcd638
talhabektas <mehmettalha.bektas@gmail.com>
2025-05-21 23:17:12 +0300

    asda

--- src/main/java/com/example/jenkkuber/JenkkuberApplication.java
+++ src/main/java/com/example/jenkkuber/JenkkuberApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JenkkuberApplication {
-//comment3
+//comment4
     public static void main(String[] args) {
         SpringApplication.run(JenkkuberApplication.class, args);
     }
