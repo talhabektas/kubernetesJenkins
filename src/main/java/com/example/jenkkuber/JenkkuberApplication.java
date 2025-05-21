40df5578e78c801a1ebe574b9ae0805ea02c6cc5
talhabektas <mehmettalha.bektas@gmail.com>
2025-05-22 01:44:53 +0300

    commit7

--- src/main/java/com/example/jenkkuber/JenkkuberApplication.java
+++ src/main/java/com/example/jenkkuber/JenkkuberApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JenkkuberApplication {
-//comment6
+//comment7
     public static void main(String[] args) {
         SpringApplication.run(JenkkuberApplication.class, args);
     }
